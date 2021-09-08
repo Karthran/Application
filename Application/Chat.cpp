0d44c2abcc4be2477f9385f09b1cdfa6dce6b9f8
Karthran <rksm29@rambler.ru>
2021-09-08 09:25:06 +0300

    Add constructor Chat(int)

--- Application/Chat.cpp
+++ Application/Chat.cpp
@@ -1,1 +1,7 @@
 #include "Chat.h"
+#include "Message.h"
+
+Chat::Chat(int max_message_num) : _max_message_num(max_message_num), _current_message_num(0)
+{
+    _message_array = new Message[_max_message_num];
+}
